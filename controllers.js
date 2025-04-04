1d96c4207621aa453aa800908b93cbb7b3e00e34
sakshi pimpale <sakshipimpale88@gmail.com>
2025-04-04 19:31:44 +0530

    updated controllers.js file

--- controllers.js
+++ controllers.js
@@ -1,34 +1,34 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-//const db = require("./config");
-const mysql = require("mysql2");
+const db = require("./config");
+//const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: 12464,
-  ssl: {
-    rejectUnauthorized: false, // Ensures SSL verification
-  },
-});
+// const db = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   port: 12464,
+//   ssl: {
+//     rejectUnauthorized: false, // Ensures SSL verification
+//   },
+// });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed: " + err.message);
-  } else {
-    console.log("Connected to MySQL Database ✅");
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Database connection failed: " + err.message);
+//   } else {
+//     console.log("Connected to MySQL Database ✅");
 
-    db.query("SHOW TABLES", (err, tables) => {
-      if (err) {
-        console.error("Error checking tables:", err);
-      } else {
-        console.log("Available tables:", tables.map(t => Object.values(t)[0]));
-      }
-    });
-  }
-});
+//     db.query("SHOW TABLES", (err, tables) => {
+//       if (err) {
+//         console.error("Error checking tables:", err);
+//       } else {
+//         console.log("Available tables:", tables.map(t => Object.values(t)[0]));
+//       }
+//     });
+//   }
+// });
 
 // User Registration
 const register = (req, res) => {
